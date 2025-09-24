07cd7a0d66be7d0767d7baa4509593695db3b93e
Axcel <ruben23ti@mahasiswa.pcr.ac.id>
2025-09-24 08:01:08 +0700

    Starter Project Laravel

---
+++ resources/views/halaman-mahasiswa-detail.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Detail</title>
+</head>
+<body>
+    <h1>Selamat Datang!</h1>
+    <p>Ini halaman detail mahasiswa</p>
+</body>
+</html>
---
+++ resources/views/halaman-mahasiswa-profil.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Profil</title>
+</head>
+<body>
+    <h1>Selamat Datang!</h1>
+    <p>Ini halaman Profil Mahasiswa</p>
+</body>
+</html>
